efcfd0c89bfa94fbb38c3fc1b0a9b7b452e64dbc
Matheus Nunes <matheusnunes261205@gmail.com>
2025-07-09 00:18:09 -0300

    adicioanado paginação.

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -27,7 +27,7 @@ const PokeCard = ({ pokedex }) => {
     <>
       {pokedex.map((item) => (
         <div key={item.id} className={styles.pokeCard}>
-          <span className={styles.pokeId}> {item.id < 10 ? `N° 000${item.id}` : item.id >= 10 && item.id < 100 ? `N° 00${item.id}` : item.id >= 100 && item.id < 1000 ? `N° 0${item.id}` : `N° ${item.id}`} </span>
+          <span className={styles.pokeId}> {item.id < 10 ? `#000${item.id}` : item.id >= 10 && item.id < 100 ? `#00${item.id}` : item.id >= 100 && item.id < 1000 ? `#0${item.id}` : `#${item.id}`} </span>
 
           <div className={styles.pokeImgContainer}>
             <img
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,11 +5,16 @@ import styles from "./Home.module.css";
 import { fetchPokedex } from "../../services/PokeAPI.js";
 import PokeCard from "../../components/PokeCard/PokeCard.jsx";
 import Spinner from "../../components/Spinner/Spinner.jsx";
+import TablePagination from '@mui/material/TablePagination';
 
 const Home = () => {
   const [pokedex, setPokedex] = useState([]);
-  const [search, setSearch] = useState('')
-  const [filterPodekex, setFilterPodekex] = useState([])
+  const [search, setSearch] = useState('');
+  const [filterPodekex, setFilterPodekex] = useState([]);
+
+  // Paginação
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   useEffect(() => {
     fetchPokedex().then((data) => setPokedex(data));
@@ -17,15 +22,35 @@ const Home = () => {
 
   useEffect(() => {
     if (pokedex.length > 0) {
-      setFilterPodekex(pokedex)
+      setFilterPodekex(pokedex);
+      setPage(0); // sempre volta para página 0 ao atualizar pokedex
     }
-  }, [pokedex])
+  }, [pokedex]);
 
   useEffect(() => {
-    const filterPokemon = pokedex.filter((pokemon) => pokemon.name.toLowerCase().includes(search.toLowerCase().trim()) || pokemon.id.toString().includes(search.trim()));
-    setFilterPodekex(filterPokemon)
-  }, [search])
+    const filterPokemon = pokedex.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(search.toLowerCase().trim()) ||
+      pokemon.id.toString().includes(search.trim())
+    );
+    setFilterPodekex(filterPokemon);
+    setPage(0); // volta para a primeira página ao pesquisar
+  }, [search, pokedex]);
 
+  // Funções de controle da paginação
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Cards da página atual
+  const currentCards = filterPodekex.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
 
   return (
     <PageBase>
@@ -33,13 +58,36 @@ const Home = () => {
         <div className={styles.searchWrapper}>
           <InputSearch setSearch={setSearch} />
         </div>
-        <div className={styles.cardWrapper}>
-          {pokedex.length === 0
-            ? <Spinner size={"80px"} />
-            : filterPodekex.length > 0
-              ? <PokeCard pokedex={filterPodekex} />
-              : <p className={styles.notFound}>Nenhum pokémon  com este nome foi encontrado!</p>}
-        </div>
+        <>
+          {pokedex.length === 0 ? (
+            <Spinner size={"80px"} />
+          ) : filterPodekex.length > 0 ? (
+            <div className={styles.cardContainer}>
+              <div className={styles.cardWrapper}>
+                <PokeCard pokedex={currentCards} />
+              </div>
+              <TablePagination
+                sx={{
+                  color: '#ffffffbd',
+                  '& .MuiTablePagination-selectLabel': {fontSize:'1.6rem'},
+                  '& .MuiTablePagination-select': { fontSize:'1.6rem'},
+                  '& .MuiTablePagination-displayedRows': {fontSize:'1.6rem',ontWeight: 'bold' },
+                  '& .MuiSvgIcon-root': {fontSize:'2rem'},
+                }}
+                component="div"
+                count={filterPodekex.length}
+                page={page}
+                onPageChange={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                rowsPerPageOptions={[10, 40, 100, 200, 500, 1025]}
+                labelRowsPerPage="Pokémons por página:"
+              />
+            </div>
+          ) : (
+            <p className={styles.notFound}>Nenhum pokémon encontrado!</p>
+          )}
+        </>
       </div>
     </PageBase>
   );
